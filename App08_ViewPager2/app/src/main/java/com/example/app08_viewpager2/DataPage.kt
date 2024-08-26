9ef6af8d6ff700ae23310b91f995b957e41045a4
zyxwvutsujin <rhcwnl1904@naver.com>
2024-08-26 12:00:01 +0900

    ViewPager02

---
+++ App08_ViewPager2/app/src/main/java/com/example/app08_viewpager2/DataPage.kt
@@ -0,0 +1,3 @@
+package com.example.app08_viewpager2
+
+data class DataPage(var color:Int, var title:String)
--- App08_ViewPager2/app/src/main/java/com/example/app08_viewpager2/MainActivity.kt
+++ App08_ViewPager2/app/src/main/java/com/example/app08_viewpager2/MainActivity.kt
@@ -7,13 +7,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.viewpager2.widget.ViewPager2
-import com.example.app08_viewpager.ViewPagerAdapter
 import com.example.app08_viewpager2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+      //  setContentView(R.layout.activity_main)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -22,25 +22,25 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         var list = mutableListOf<DataPage>(
-            DataPage(Color.rgb(221, 174, 184), "1 Page"),
-            DataPage(Color.rgb(231, 193, 157), "2 Page"),
-            DataPage(Color.rgb(252, 212, 129), "3 Page"),
-            DataPage(Color.rgb(223, 240, 215), "4 Page"),
-            DataPage(Color.rgb(223, 230, 239), "5 Page"),
-            DataPage(Color.rgb(157, 182, 239), "6 Page")
+            DataPage(Color.RED, "1 page"),
+            DataPage(Color.BLUE, "2 page"),
+            DataPage(Color.GRAY, "3 page"),
+            DataPage(Color.BLACK, "4 page"),
+            DataPage(Color.YELLOW, "5 page"),
+            DataPage(Color.CYAN, "6 page")
         )
         binding.viewPager2.adapter = ViewPagerAdapter(list)
-        binding.btnToggle.setOnClickListener{
+        binding.btnToggle.setOnClickListener {
             when(binding.viewPager2.orientation){
-                ViewPager2.ORIENTATION_VERTICAL-> {
-                 binding.btnToggle.text = "가로로 슬라이드"
-                 binding.viewPager2.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+                ViewPager2.ORIENTATION_VERTICAL->{
+                    binding.btnToggle.text = "가로로 슬라이드"
+                    binding.viewPager2.orientation = ViewPager2.ORIENTATION_HORIZONTAL
                 }
-                ViewPager2.ORIENTATION_HORIZONTAL-> {
-                    binding.btnToggle.text = "세로로 슬라이드"
-                    binding.viewPager2.orientation = ViewPager2.ORIENTATION_VERTICAL
+                ViewPager2.ORIENTATION_HORIZONTAL->{
+                   binding.btnToggle.text="세로로 슬라이드"
+                   binding.viewPager2.orientation = ViewPager2.ORIENTATION_VERTICAL
                 }
             } //when
-        } //btnToggle
+        }// btnToggle
     }
 }
